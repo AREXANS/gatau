82a00f70779663cdde59e86ca24435262644e820
AREXANS <89494133+AREXANS@users.noreply.github.com>
2022-06-01 03:19:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ second: 'numeric'
 
 function pickRandom(list) {
   return list[Math.floor(Math.random() * list.length)]
-}global.owner = ['6282146092695'] // Put your number here
+}global.owner = ['6289518030035'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
@@ -64,15 +64,15 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.stiker_wait = 'Stiker sedang dibuat'
-global.packname = 'Rika'
-global.author = '>////<'
-
+global.packname = 'Created By'
+global.author = 'Aryaa ×፝֟͜×'
+/
 
 
 
 
 ig: 'ahmdlui'
-global.wm = 'SoikemMd'
+global.wm = 'Aryaa ×፝֟͜×'
 global.watermark = 'wm'
 global.botdate = '╭──────────═┅═─────────\n│◪ ${time}\n┊◪ ${week} ${date}\n╰──────────═┅═─────────'
 global.media = 'https://telegra.ph/file/e001198ced5650ef53d47.jpg'
